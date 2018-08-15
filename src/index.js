58dd6c6e362dab04d5d33c5279a7e723d7b06cad
Ruben <rubennm@gmail.com>
2018-08-15 12:52:01 -0500

    setting api ingestion url as process variable or defaulting

--- src/index.js
+++ src/index.js
@@ -138,9 +138,10 @@ const handleCloudTrailLogs = (event, context, lintEnv) => {
 
 const handler = (event, context) => {
   const apiToken = process.env.LogIntelligence_API_Token;
+  const ingestionUrl = process.env.LogIntelligence_API_Url || 'https://data.cloud.symphony-dev.com/le-mans/v1/streams/ingestion-pipeline-stream';
   const lintEnv = new LIntHttpEnv(
     'Bearer ' + apiToken,
-    'https://data.cloud.symphony-dev.com/le-mans/v1/streams/ingestion-pipeline-stream',
+    ingestionUrl
   );
 
   if (event.awslogs) {
