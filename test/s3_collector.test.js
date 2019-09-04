c943901028481e98d9f63a7e37b52f8a9bedb2cc
Arpit Singhal <sarpit@vmware.com>
2019-09-04 12:32:56 +0530

    Enhanced S3 Data test case Signed-off-by: Arpit Singhal <sarpit@vmware.com>

--- test/s3_collector.test.js
+++ test/s3_collector.test.js
@@ -6,6 +6,7 @@ SPDX-License-Identifier: MIT
 const { lintTestEnv } = require('../test/helper.test');
 const { s3SampleData } = require('./sample_files/s3_testdata');
 const { s3SampleFileContent } = require('./sample_files/s3_testdata');
+const { s3SampleRawLogs } = require('./sample_files/s3_testdata');
 
 const {
   S3HttpCollector,
@@ -27,9 +28,16 @@ describe('S3HttpCollector', () => {
   });
 
   describe('processS3Data', () => {
-    it('should process data JSON', () => {
+    it('should process JSON data', () => {
       const logsJson = s3SampleFileContent();
       const data = collector.processS3Data('test', JSON.stringify(logsJson));
+      expect(data)
+        .toMatchSnapshot();
+    });
+
+    it('should process PlainText data', () => {
+      const rawLogs = s3SampleRawLogs();
+      const data = collector.processS3Data('test', JSON.stringify(rawLogs));
       expect(data)
         .toMatchSnapshot();
     });
